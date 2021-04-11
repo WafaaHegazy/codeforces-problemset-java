dfd7ba3b13f3a69ab977a97d12b6316ff0f84fed
Wafaa Hegazy <wafaa.hegazy@mondia.com>
2021-04-11 22:01:00 +0200

    add next round

--- NextRound/src/com/problemSolving/Main.java
+++ NextRound/src/com/problemSolving/Main.java
@@ -1,5 +1,7 @@
 package com.problemSolving;
 
+import java.util.Scanner;
+
 /**
  * https://codeforces.com/problemset/problem/158/A
  *
@@ -8,6 +10,32 @@ package com.problemSolving;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        Integer noOfPlayers = scanner.nextInt();
+        Integer index = scanner.nextInt();
+        Integer[] scores = new Integer[noOfPlayers];
+        Integer counter = 0;
+        for (int j = 0; j < noOfPlayers; j++) {
+            scores[j] = scanner.nextInt();
+        }
+        if (index > noOfPlayers) {
+            System.out.println(0);
+        }
+        Integer remainingPlayers = getNoOfPlayerInNextRound(scores, index, noOfPlayers);
+        for (int k = 0; k < remainingPlayers; k++) {
+            if (scores[k] > 0) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
+    }
+
+    private static Integer getNoOfPlayerInNextRound(Integer[] scores, Integer index, Integer noOfPlayers) {
+        for (int i = index - 1; i < noOfPlayers; i++) {
+            if (scores[i] < scores[index-1]) {
+                return i;
+            }
+        }
+        return noOfPlayers;
     }
 }
